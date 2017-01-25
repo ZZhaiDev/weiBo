df493bae353bfe5b854a08c9a16d7987cea04c92
zijia <zijia@zijiadeiMac.local>
2017-01-25 00:39:05 -0800

    user.swift updated,  and avatar images uploaded

--- weiBo/weiBo/classes/Home/HomeModel/User.swift
+++ weiBo/weiBo/classes/Home/HomeModel/User.swift
@@ -11,8 +11,31 @@ import UIKit
 class User: NSObject {
     
     // MARK:- 属性
-    var profile_image_url : String?
-    var screen_name : String?
+    var profile_image_url : String?     //用户头像
+    var screen_name : String?           //用户昵称
+    
+    var verified_type : Int = -1 {      //用户认证类型
+        didSet{
+            switch verified_type {
+            case 0:
+                verifiedImage = UIImage(named: "avatar_vip")
+            case 2, 3, 5:
+                verifiedImage = UIImage(named: "avatar_enterprise_vip")
+            case 220:
+                verifiedImage = UIImage(named: "avatar_grassroot")
+            default:
+                verifiedImage = nil
+            }
+        }
+    }
+    
+    var mbrank : Int = 0 {              //用户会员等级
+        didSet{
+            if mbrank > 0 && mbrank <= 6 {
+                vipImage = UIImage(named: "common_icon_membership_level\(mbrank)")
+            }
+        }
+    }
     
     // MARK:- 对数据处理
     var verifiedImage : UIImage?
